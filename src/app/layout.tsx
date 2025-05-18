08ea52eb5552079d7d36e85c54f1e5cdc9650565
cjmpmd <71334484+cjmpmd@users.noreply.github.com>
2025-05-18 15:16:28 -0400

    style: added darkmode context and layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { ThemeProvider } from "../../contexts/theme-provider";
+import { ThemeProvider } from "@/contexts/theme-provider";
 
 export const metadata: Metadata = {
   title: "Create Next App",
